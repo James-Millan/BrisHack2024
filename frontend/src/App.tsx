b243d8d85cd1dc7acd764841a16e6364bb3bbbeb
Matthew Swann <cv20549@bristol.ac.uk>
2024-02-25 14:59:47 +0000

    added button!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function App() {
+
+  const [bearerToken, setBearerToken] = useState<string | null>(null);
+
   return (
     <div className="App bg-gradient-to-t to-green-500 from-green-900 h-screen w-screen">
-      <a className="cursor-pointer bg-green-800 text-gray-100 p-5 rounded-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" href="/auth">Login with Spotify</a>
+      {bearerToken}
+      {bearerToken === null &&
+      // <a className="cursor-pointer bg-green-800 text-gray-100 p-5 rounded-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" href="/auth">Login with Spotify</a>
+      <div className="cursor-pointer bg-green-800 text-gray-100 p-5 rounded-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" onClick={() => setBearerToken("Hi")}>Login with Spotify</div>
+      }
     </div>
   );
 }
