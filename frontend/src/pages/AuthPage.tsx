373180f80d7e842cada8ff926320bcb765f4c213
georgewigley <georgewigley123@gmail.com>
2024-02-25 15:46:01 +0000

    Added React router and 404 page, renamed App to Auth Page

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function App() {
+function AuthPage() {
 
   const [bearerToken, setBearerToken] = useState<string | null>(null);
 
@@ -15,4 +15,4 @@ function App() {
   );
 }
 
-export default App;
+export default AuthPage;
