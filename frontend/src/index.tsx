dae004bf91483311781d40bdb33c6a9cbc387d37
Matthew Swann <cv20549@bristol.ac.uk>
2024-02-25 19:58:30 +0000

    auth is done

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,21 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AuthPage from './pages/AuthPage';
-import {Routes, Route, BrowserRouter} from "react-router-dom"
-import NotFound from "../src/pages/NotFound"
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import NotFound from "../src/pages/NotFound";
+import { useState } from 'react';
+
+const App = () => {
+
+  const [bearerToken, setBearerToken] = useState<string | null>(null);
+
+  return <React.StrictMode>
+  <BrowserRouter>
+      <Routes>
+          <Route index element={<AuthPage bearerToken={bearerToken} setBearerToken={setBearerToken} />} />
+          <Route path="/callback" element={<AuthPage bearerToken={bearerToken} setBearerToken={setBearerToken} />} />
+          {/*<Route path="about" element={<About />} />*/}
+          {/*<Route path="dashboard" element={<Dashboard />} />*/}
+          <Route path="*" element={<NotFound />} />
+      </Routes>
+  </BrowserRouter>
+</React.StrictMode>
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <Routes>
-              <Route index element={<AuthPage />} />
-              {/*<Route path="about" element={<About />} />*/}
-              {/*<Route path="dashboard" element={<Dashboard />} />*/}
-              <Route path="*" element={<NotFound />} />
-          </Routes>
-      </BrowserRouter>
-  </React.StrictMode>
+  <App />
 );
