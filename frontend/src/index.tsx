62155eec63aca0b3de7214e54d8488b1c1b059f7
georgewigley <georgewigley123@gmail.com>
2024-02-25 20:10:46 +0000

    V1 Completed

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AuthPage from './pages/AuthPage';
 import {Routes, Route, BrowserRouter} from "react-router-dom"
 import NotFound from "../src/pages/NotFound"
 import FormPage from "./pages/Form";
-import BGTile from "./components/background";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,7 +12,7 @@ root.render(
   <React.StrictMode>
       <BrowserRouter>
           <Routes>
-              <Route index element={<AuthPage />} />
+              {/*<Route index element={<AuthPage />} />*/}
               {/*<Route path="about" element={<About />} />*/}
               <Route path="/start" element={<FormPage />} />
               <Route path="*" element={<NotFound />} />
