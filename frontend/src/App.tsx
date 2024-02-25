0aa3fcde0327cf932bf1a5196fea9f8719294620
Matthew Swann <cv20549@bristol.ac.uk>
2024-02-25 14:42:19 +0000

    basic welcome page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,7 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code className='text-red-900'>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gradient-to-t to-green-500 from-green-900 h-screen w-screen">
+      <a className="cursor-pointer bg-green-800 text-gray-100 p-5 rounded-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" href="/auth">Login with Spotify</a>
     </div>
   );
 }
