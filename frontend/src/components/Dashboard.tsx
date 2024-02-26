889945feef4e4337ea232dc853fd6d80cb597916
georgewigley <georgewigley123@gmail.com>
2024-02-26 05:38:14 +0000

    Animate

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -13,84 +13,118 @@ function toHHMMSS(num: number) {
     return `${hours}h ${minutes}m ${seconds}s`;
 };
 
-const Dashboard = ({APIData} : {APIData : any}) => {
+const Dashboard = ({APIData}: { APIData: any }) => {
 
 
     return (
-            <Box display={"flex"} alignItems={"center"} justifyContent={"space-evenly"} width={"100vw"} height={"100vh"}>
-                <Card style={{backgroundColor:"#131313", width: '50%', height: '90%', display: "flex", flexDirection:"column", justifyContent:"space-between", padding:"2%", borderRadius:"12px"}}>
+        <Box display={"flex"} alignItems={"center"} justifyContent={"space-evenly"} width={"100vw"} height={"100vh"}>
+            <motion.div
+                initial={{opacity: 0, y: -50}}
+                animate={{opacity: 1, y: 0}}
+                transition={{duration: 0.3, delay: 0}}
+                style={{
+                    width: "50%",
+                    height: "90%"
+                }}
+            >
+                <Card style={{
+                    backgroundColor: "#131313",
+                    width: '100%',
+                    height: '100%',
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    padding: "2%",
+                    borderRadius: "12px"
+                }}>
                     <Map APIData={APIData}/>
                 </Card>
-                <Box width={"20%"} gap={5} height={"90%"} display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
-                    <Box width={"100%"} gap={4} height={"100%"} style={{backgroundColor:"#232323", borderRadius:"12px"}} p={5} display={"flex"} justifyContent={"top"} alignItems={"center"} flexDirection={"column"}>
-                        <motion.div
-                            initial={{opacity: 0, x: -50}}
-                            animate={{opacity: 1, x: 0}}
-                            transition={{duration: 0.3, delay: 0}}
-                        >
-                            <Typography variant={"h5"} color={"#ffffff"} textAlign={"center"} >
-                                Distance
-                            </Typography>
-                        </motion.div>
-                        <motion.div
-                            initial={{opacity: 0, scale: 0}}
-                            animate={{opacity: 1, scale: 1}}
-                            transition={{duration: 0.3, delay: 0.2}}
-                        >
-                            <Typography variant={"h3"} color={"primary"} textAlign={"center"} >
-                                {`${APIData[`distance`] / 1000 } km`}
-                            </Typography>
-                        </motion.div>
-                    </Box>
-                    <Box width={"100%"} gap={4} height={"100%"} style={{backgroundColor:"#232323", borderRadius:"12px"}} p={5} display={"flex"} justifyContent={"top"} alignItems={"center"} flexDirection={"column"}>
-                        <motion.div
-                            initial={{opacity: 0, x: -50}}
-                            animate={{opacity: 1, x: 0}}
-                            transition={{duration: 0.3, delay: 0.3}}
-                        >
-                            <Typography variant={"h5"} color={"#ffffff"} textAlign={"center"} >
-                                Duration
-                            </Typography>
-                        </motion.div>
-                        <motion.div
-                            initial={{opacity: 0, scale: 0}}
-                            animate={{opacity: 1, scale: 1}}
-                            transition={{duration: 0.3, delay: 0.5}}
-                        >
-                            <Typography variant={"h3"} color={"primary"} textAlign={"center"} >
-                                {` ${toHHMMSS(APIData[`duration`])} `}
-                            </Typography>
-                        </motion.div>
-                    </Box><Box width={"100%"} gap={4} height={"100%"} style={{backgroundColor:"#232323", borderRadius:"12px"}} p={5} display={"flex"} justifyContent={"top"} alignItems={"center"} flexDirection={"column"}>
+            </motion.div>
+            <Box width={"20%"} gap={5} height={"90%"} display={"flex"} flexDirection={"column"}
+                 justifyContent={"center"} alignItems={"center"}>
+                <Box width={"100%"} gap={4} height={"100%"} style={{backgroundColor: "#232323", borderRadius: "12px"}}
+                     p={5} display={"flex"} justifyContent={"top"} alignItems={"center"} flexDirection={"column"}>
                     <motion.div
-                        initial={{opacity: 0, x: -50}}
-                        animate={{opacity: 1, x: 0}}
-                        transition={{duration: 0.3, delay: 0.7}}
+                        initial={{opacity: 0, y: -50}}
+                        animate={{opacity: 1, y: 0}}
+                        transition={{duration: 0.3, delay: 0}}
                     >
-                        <Typography variant={"h5"} color={"#ffffff"} textAlign={"center"} >
-                            Vibes
+                        <Typography variant={"h5"} color={"#ffffff"} textAlign={"center"}>
+                            Distance
                         </Typography>
                     </motion.div>
                     <motion.div
-                        initial={{opacity: 0, scale: 0}}
-                        animate={{opacity: 1, scale: 1}}
-                        transition={{duration: 0.3, delay: 0.9}}
+                        initial={{opacity: 0, y: -50}}
+                        animate={{opacity: 1, y: 0}}
+                        transition={{duration: 0.3, delay: 0.1}}
                     >
-                        <Typography variant={"h3"} color={"primary"} textAlign={"center"} >
-                            ?
+                        <Typography variant={"h3"} color={"primary"} textAlign={"center"}>
+                            {`${APIData[`distance`] / 1000} km`}
                         </Typography>
                     </motion.div>
                 </Box>
-                </Box>
+                <Box width={"100%"} gap={4} height={"100%"} style={{backgroundColor: "#232323", borderRadius: "12px"}}
+                     p={5} display={"flex"} justifyContent={"top"} alignItems={"center"} flexDirection={"column"}>
+                    <motion.div
+                        initial={{opacity: 0, y: -50}}
+                        animate={{opacity: 1, y: 0}}
+                        transition={{duration: 0.3, delay: 0.15}}
+                    >
+                        <Typography variant={"h5"} color={"#ffffff"} textAlign={"center"}>
+                            Duration
+                        </Typography>
+                    </motion.div>
+                    <motion.div
+                        initial={{opacity: 0, y: -50}}
+                        animate={{opacity: 1, y: 0}}
+                        transition={{duration: 0.3, delay: 0.25}}
+                    >
+                        <Typography variant={"h3"} color={"primary"} textAlign={"center"}>
+                            {` ${toHHMMSS(APIData[`duration`])} `}
+                        </Typography>
+                    </motion.div>
+                </Box><Box width={"100%"} gap={4} height={"100%"}
+                           style={{backgroundColor: "#232323", borderRadius: "12px"}} p={5} display={"flex"}
+                           justifyContent={"top"} alignItems={"center"} flexDirection={"column"}>
+                <motion.div
+                    initial={{opacity: 0, y: -50}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{duration: 0.3, delay: 0.3}}
+                >
+                    <Typography variant={"h5"} color={"#ffffff"} textAlign={"center"}>
+                        Vibes
+                    </Typography>
+                </motion.div>
+                <motion.div
+                    initial={{opacity: 0, y: -50}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{duration: 0.3, delay: 0.35}}
+                >
+                    <Typography variant={"h3"} color={"primary"} textAlign={"center"}>
+                        ?
+                    </Typography>
+                </motion.div>
+            </Box>
+            </Box>
+            <motion.div
+                initial={{opacity: 0, y: -50}}
+                animate={{opacity: 1, y: 0}}
+                transition={{duration: 0.3, delay: 0.3}}
+                style={{
+                    width: "25%",
+                    height: "90%"
+                }}
+            >
                 <iframe
-                    style={{borderRadius:"12px"}}
+                    style={{borderRadius: "12px"}}
                     src={`https://open.spotify.com/embed/playlist/${APIData["playlistId"]}?utm_source=generator&theme=0`}
-                    width="25%"
-                    height="90%"
+                    width="100%"
+                    height="100%"
                     allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                     loading="lazy">
                 </iframe>
-            </Box>
+            </motion.div>
+        </Box>
     )
 }
 
